6fc00c59b1b7878f7d31095a933b01101bcd825b
user <me@mail.com>
2020-06-04 15:36:38 -0700

    Initial Commit

---
+++ SecureText/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecureText
+//
+//  Created by RnD on 6/4/20.
+//  Copyright © 2020 RnD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
